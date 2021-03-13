568518ca0400cf916e82f1decb98e32d4b19c386
Robert Szachnowski <rafeq1241@gmail.com>
2021-03-13 11:26:34 +0100

    Initialize project

---
+++ src/ReBus.Serializer.XML/Extensions/RebusSerializerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rebus.Config;
+using Rebus.Serialization;
+
+namespace ReBus.Serializer.XML.Extensions
+{
+    public static class RebusSerializerExtensions
+    {
+        public static void UseXmlSerializing(this StandardConfigurer<ISerializer> configurer)
+        {
+            if (configurer == null)
+            {
+                throw new ArgumentNullException(nameof(configurer));
+            }
+
+            configurer.Register(r=> new XmlSerializer());
+        }
+    }
+}
---
+++ src/ReBus.Serializer.XML/XmlSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Rebus.Messages;
+using Rebus.Serialization;
+
+namespace ReBus.Serializer.XML
+{
+    public class XmlSerializer : ISerializer
+    {
+        public Task<TransportMessage> Serialize(Message message) => throw new System.NotImplementedException();
+
+        public Task<Message> Deserialize(TransportMessage transportMessage) => throw new System.NotImplementedException();
+    }
+}

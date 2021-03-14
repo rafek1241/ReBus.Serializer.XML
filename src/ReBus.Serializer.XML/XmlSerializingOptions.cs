07df3428b24feb7ae2d111b696a4462266131500
Robert Szachnowski <rafeq1241@gmail.com>
2021-03-14 22:45:37 +0100

    Add options for defining base type namespaces attribute. Add options to include base type attributes within root container.

--- src/ReBus.Serializer.XML/XmlSerializingOptions.cs
+++ src/ReBus.Serializer.XML/XmlSerializingOptions.cs
@@ -5,9 +5,10 @@ namespace ReBus.Serializer.XML
     public class XmlSerializingOptions
     {
         public string RootName { get; set; } = "Messages";
-        public bool IncludeNamespace { get; set; }
+        public bool IncludeNamespace { get; set; } = true;
+        public bool IncludeBaseTypeNamespaces { get; set; } = true;
         public string DefaultNamespacePrefix { get; set; } = "http://tempuri.org";
-
+        public string BaseTypeNamespaceAttributeName { get; set; } = "baseType";
         public Encoding Encoding { get; set; } = null;
     }
 }
